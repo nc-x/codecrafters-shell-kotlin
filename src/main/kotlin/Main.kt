9c131c445c18041c301897302db4792ad40eebd2
Neelesh Chandola <neelesh.chandola@outlook.com>
2024-11-01 19:12:05 +0000

    type builtin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
+import Command.Echo
+import Command.Exit
+import Command.None
+import Command.Type
+import Command.Unknown
 import kotlin.system.exitProcess
 
 fun main() {
@@ -10,16 +15,17 @@ fun main() {
 
 fun handleCommands(input: String) {
     val (cmd, args) = input.split("\\s+".toRegex()).let {
-        it[0] to it.drop(1)
+        Command.from(it[0]) to it.drop(1)
     }
-    when (cmd) {
-        "" -> {}
 
-        "echo" -> {
-            println(input.substringAfter("echo "))
+    when (cmd) {
+        None -> {}
+
+        Echo -> {
+            println(input.substringAfter("${cmd.value} "))
         }
 
-        "exit" -> {
+        Exit -> {
             if (args.size != 1 || args[0].toIntOrNull() == null) {
                 println("invalid arguments")
                 return
@@ -28,8 +34,37 @@ fun handleCommands(input: String) {
             exitProcess(status)
         }
 
-        else -> {
-            println("$cmd: command not found")
+        Type -> {
+            if (args.size != 1) {
+                println("invalid arguments")
+                return
+            }
+            val builtin = Command.entries.firstOrNull { it.value == args[0] }
+            if (builtin != null) {
+                println("${args[0]} is a shell builtin")
+            } else {
+                println("${args[0]}: not found")
+            }
         }
+
+        is Unknown -> {
+            println("${cmd.value}: command not found")
+        }
+    }
+}
+
+sealed class Command(val value: String) {
+    object None : Command("")
+    object Echo : Command("echo")
+    object Exit : Command("exit")
+    object Type : Command("type")
+    class Unknown(value: String) : Command(value)
+
+    companion object {
+        val entries: List<Command> =
+            Command::class.sealedSubclasses.mapNotNull { it.objectInstance }
+
+        fun from(value: String): Command =
+            entries.firstOrNull { it.value == value } ?: Unknown(value)
     }
 }
