09380e09d3f3434f6e131d3391ce7ac0bc9dbd47
Neelesh Chandola <neelesh.chandola@outlook.com>
2024-11-01 18:51:13 +0000

    exit command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,30 @@
+import kotlin.system.exitProcess
+
 fun main() {
     while (true) {
         print("$ ")
         val cmd = readln()
-        println("$cmd: command not found")
+        handleCommands(cmd)
+    }
+}
+
+fun handleCommands(cmd: String) {
+    val (cmd, args) = cmd.split("\\s+".toRegex()).let {
+        it[0] to it.drop(1)
+    }
+    when (cmd) {
+        "" -> {}
+        "exit" -> {
+            if (args.size != 1 || args[0].toIntOrNull() == null) {
+                println("invalid arguments")
+                return
+            }
+            val status = args[0].toInt()
+            exitProcess(status)
+        }
+
+        else -> {
+            println("$cmd: command not found")
+        }
     }
 }
